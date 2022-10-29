d86cb3c574e44b32fe0676048d901642879f30b6
Md Danish Akhtar <102619612+DanishAkhtar26@users.noreply.github.com>
2022-10-29 13:57:08 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,81 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  let bgc = "";
+  if(count >= 10){
+    bgc = "bgc-blue";
+  }
+  else if(count <= -10){
+    bgc = "bgc-red";
+  }
+
+  const handleInc = (event) => {
+    if(count >= 10){
+      event.currentTarget.disabled = true;
+    }
+    else{
+      setCount(count + 1);
+    }
+  };
+
+  const handleDec = (event) => {
+    if(count<= -10){
+      event.currentTarget.disabled = true;
+    }
+    else{
+      setCount(count - 1);
+    }
+  };
+  const handleReset = (event) => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <div className={bgc}>
+        <h1>You've clicked the button {count} times.</h1>
+        <button onClick={handleInc}>Increment</button>
+        <span> </span>
+        <button onClick={handleDec}>Decrement</button>
+        <span> </span>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
