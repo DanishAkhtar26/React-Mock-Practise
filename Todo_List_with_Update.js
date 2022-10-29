e8ffcc2eaef264416094b7c951ec7229c10467ba
Md Danish Akhtar <102619612+DanishAkhtar26@users.noreply.github.com>
2022-10-29 14:36:01 +0530

    Update Todo_List_with_Update.js

--- Todo_List_with_Update.js
+++ Todo_List_with_Update.js
@@ -1,3 +1,35 @@
+App.js file content :
+
+import React from "react";
+import styles from "./styles.module.css";
+
+import Todo from "./Todo";
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.main}>react todo app</h1>
+
+      <Todo />
+    </div>
+  );
+}
+
+Css :
+.App {
+  font-family: sans-serif;
+  text-align: center;
+}
+
+.main {
+  background-color: #04160b;
+  color: white;
+}
+
+
+
+Todo.jsx file content :
+
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
